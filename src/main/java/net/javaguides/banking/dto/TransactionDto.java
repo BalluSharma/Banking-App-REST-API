c29f747957b02b12b2ecf19ec9d13c74e7c0ae10
Ballu Sharma <ballusharma6366@gmail.com>
2025-07-20 13:46:09 +0530

    Banking App Using Spring Boot

---
+++ src/main/java/net/javaguides/banking/dto/TransactionDto.java
@@ -0,0 +1,11 @@
+package net.javaguides.banking.dto;
+
+import java.time.LocalDateTime;
+
+public record TransactionDto(Long id,
+                             Long accountId,
+                             double amount,
+                             String transactionType,
+                             LocalDateTime timestamp) {
+
+}
